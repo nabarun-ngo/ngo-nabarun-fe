93ec31e65995fb19e813762761e5325aefea1b3d
Souvik Sarkar <souviksarrkar362@gmail.com>
2026-01-28 01:40:15 +0530

    feat: Add Report Dashboard with finance report tab, document linking, and new API client services.

--- src/app/feature/dashboard/report-dashboard/report-dashboard.component.ts
+++ src/app/feature/dashboard/report-dashboard/report-dashboard.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AppRoute } from 'src/app/core/constant/app-routing.const';
 import { NavigationButtonModel } from 'src/app/shared/components/generic/page-navigation-buttons/page-navigation-buttons.component';
 import { SharedDataService } from 'src/app/core/service/shared-data.service';
-import { DocumentCategory } from 'src/app/shared/components/document-link/document-link.model';
-import { ReportService } from '../services/report.service';
 import { KeyValue } from 'src/app/shared/model/key-value.model';
-import { firstValueFrom } from 'rxjs';
-import { saveAs } from 'src/app/core/service/utilities.service';
+import { StandardTabbedDashboard } from 'src/app/shared/utils/standard-tabbed-dashboard';
+import { TabComponentInterface } from 'src/app/shared/interfaces/tab-component.interface';
+import { ActivatedRoute } from '@angular/router';
+import { FinanceReportTabComponent } from './finance-report-tab/finance-report-tab.component';
+import { ModalService } from 'src/app/core/service/modal.service';
+import { SearchAndAdvancedSearchModel } from 'src/app/shared/model/search-and-advanced-search.model';
+import { Validators } from '@angular/forms';
+import { ReportService } from '../services/report.service';
+import { SearchAndAdvancedSearchFormComponent } from 'src/app/shared/components/search-and-advanced-search-form/search-and-advanced-search-form.component';
+import { date, saveAs } from 'src/app/core/service/utilities.service';
+
+type reportTabs = 'financeReports' | 'interimReports';
 
 @Component({
   selector: 'app-report-dashboard',
   templateUrl: './report-dashboard.component.html',
   styleUrls: ['./report-dashboard.component.scss']
 })
-export class ReportDashboardComponent implements OnInit {
+export class ReportDashboardComponent extends StandardTabbedDashboard<reportTabs, KeyValue[]> {
+
+  @ViewChild(FinanceReportTabComponent) financeReportTab!: FinanceReportTabComponent;
+
+  protected override tabMapping: reportTabs[] = ['financeReports', 'interimReports'];
 
   protected navigations: NavigationButtonModel[] = [
     {
@@ -22,61 +34,82 @@ export class ReportDashboardComponent implements OnInit {
     }
   ];
 
-  financeReports: DocumentCategory[] = [];
-
-
   constructor(
     private sharedDataService: SharedDataService,
-    private reportService: ReportService) {
+    protected override route: ActivatedRoute,
+    protected modalService: ModalService,
+    private reportService: ReportService
+  ) {
+    super(route);
   }
 
-
-  ngOnInit(): void {
+  protected override onInitHook(): void {
     this.sharedDataService.setPageName(`Reports`);
-    this.reportService.listActiveReports().subscribe(data => {
-      data.forEach(async d => {
-        this.financeReports.push({
-          name: d.displayValue,
-          documents: await this.listDocuments(d.key)
-        })
+  }
+
+  protected override onAfterViewInitHook(): void {
+    this.getActiveComponent(this.getCurrentTab())?.loadData();
+  }
+
+  protected override get tabComponents(): { [key in reportTabs]?: TabComponentInterface<any> } {
+    return {
+      financeReports: this.financeReportTab,
+      interimReports: undefined
+    };
+  }
+
+  protected override get defaultTab(): reportTabs {
+    return 'financeReports';
+  }
+
+  protected override onTabChangedHook(): void {
+  }
+
+  generateInterimReport() {
+    this.reportService.listFinanceReports().subscribe(reportList => {
+      const report = this.getReportSearch(reportList);
+      let modal = this.modalService.openComponentDialog(SearchAndAdvancedSearchFormComponent,
+        report,
+        {
+          height: 290,
+          width: 700,
+        });
+      modal.componentInstance.onSearch.subscribe(data => {
+        if (data.reset) {
+          modal.close();
+        }
+        else {
+          this.reportService.generateInterimReport(data.value.reportName).subscribe((blob) => {
+            modal.close();
+            data
+            const fileName = `Interim_Report_${reportList.find(r => r.key === data.value.reportName)?.displayValue}_${date(new Date(), 'yyyyMMddHHmmss')}`;
+            saveAs(blob, fileName);
+          });
+        }
       })
-    })
-  }
-
-  private async listDocuments(key: string): Promise<KeyValue[]> {
-    const data = await firstValueFrom(this.reportService.getReports(key))
-    return data.map(d => {
-      return {
-        key: d.id,
-        displayValue: d.fileName,
-        description: d.fileName
-      } as KeyValue
-    })
-  }
-
-
-
-  onDocumentClicked($event: { doc: KeyValue, categoryName: string }) {
-    console.log($event);
-    this.reportService.downloadReports($event.doc.key!).subscribe((response) => {
-      const blob = response;
-
-      // 2. Create a temporary URL for the blob
-      const url = window.URL.createObjectURL(blob!);
-
-      // 3. Create a hidden <a> tag and click it
-      const link = window.document.createElement('a');
-      link.href = url;
-      link.download = $event.doc.description!;
-      window.document.body.appendChild(link);
-      link.click();
-
-      // 4. Cleanup
-      window.document.body.removeChild(link);
-      window.URL.revokeObjectURL(url);
-    }, error => {
-      console.error('Download failed', error);
     });
   }
 
+  private getReportSearch(reportList: KeyValue[]) {
+    return {
+      normalSearchPlaceHolder: '',
+      showOnlyAdvancedSearch: true,
+      advancedSearch: {
+        buttonText: { search: 'Generate', close: 'Close' },
+        title: 'Generate Interim Report',
+        searchFormFields: [{
+          formControlName: 'reportName',
+          inputModel: {
+            html_id: 'report_name',
+            inputType: 'text',
+            tagName: 'input',
+            autocomplete: true,
+            placeholder: 'Select Report',
+            selectList: reportList
+          },
+          validations: [Validators.required]
+        }]
+      }
+    };
+  }
 }
